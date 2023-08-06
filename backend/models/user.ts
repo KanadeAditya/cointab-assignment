8f60e03b28e94d35f430feb0b3bdc5b9b1d33b0a
Aditya Kanade <110182805+KanadeAditya@users.noreply.github.com>
2023-08-06 16:54:54 +0530

    backend

---
+++ backend/models/user.ts
@@ -0,0 +1,86 @@
+'use strict';
+import {Model, UUIDV4} from 'sequelize';
+
+interface UserAttributes{
+  id:string,
+  name:string,
+  username:string,
+  email:string,
+  image:string,
+  address:string,
+  password:string,
+  birthday:string,
+  gender:string,
+  phonenumber:string
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class User extends Model<UserAttributes> implements UserAttributes {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    id!:string;
+    name!:string;
+    username!:string;
+    email!:string;
+    image!:string;
+    address!:string;
+    password!:string;
+    birthday!:string;
+    gender!:string;
+    phonenumber!:string
+    static associate(models: any) {
+      // define association here
+    }
+  }
+  User.init({
+    id: {
+      type: DataTypes.UUID,
+      allowNull:false,
+      defaultValue : UUIDV4,
+      primaryKey:true
+    },
+    name: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    username: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    email: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    image: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    address: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    password: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    birthday: {
+      type:DataTypes.DATE,
+      allowNull:false
+    },
+    gender: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    phonenumber:{
+      type:DataTypes.STRING,
+      allowNull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
